f20af2a7d36bc87911ae2dd8749ef54f4c728934
thidkyar <thidkyar@gmail.com>
2018-10-18 23:10:19 -0400

    add entire work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      board: Array(9).fill(null)
+    };
+  }
+
+  clickHandler = i => {
+    console.log(i);
+    let newBoard = this.state.board
+    newBoard[i] = 'X'
+    this.setState({board: newBoard})
+  }; 
+
+  renderbox(e) {
+    console.log(e);
+    return this.state.board.map((box, i) => {
+      return (
+        <div key={i} className="box" onClick={() => this.clickHandler(i)}>
+          {box}
+        </div>
+      );
+    });
+  }
   render() {
+    console.log(this.state);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Tic Tac Toe</h1>
+        <div>
+          <p>Select your symbol</p>
+          <button id="x" onClick={this.buttonClickHandler}>
+            X
+          </button>
+          <button id="o" onClick={this.buttonClickHandler}>
+            O
+          </button>
+        </div>
+        <div className="board">{this.renderbox()}</div>
       </div>
     );
   }
